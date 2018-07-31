ddccf97e253b1a266c52907169fc33743cd9e138
Asif Sharif <asif-sharif@hotmail.com>
2018-07-31 19:50:33 +0500

    Sandbox commit

--- src/components/about/index.js
+++ src/components/about/index.js
@@ -1,11 +1,12 @@
-
-import React from 'react';
+import React from "react";
 
 export default () => (
   <div>
     <h1>About Us</h1>
-    <p>This is the sample code to understand all the moving parts of React & Redux including Action, Action creators,Redux Thunk, Reducers </p>
+    <p>
+      This is the sample code to understand all the moving parts of React &
+      Redux including Action, Action creators,Redux Thunk, Reducers{" "}
+    </p>
+    <p> a addition from sandbox ! </p>
   </div>
-)
-
- 
+);
